9e0009d4089603906a2a0d3862cf1b56a078be2d
Ammar <ammar@pop-os.localdomain>
2021-02-20 15:21:44 +0300

    Edite styling

--- src/lib/commirce.tsx
+++ src/lib/commirce.tsx
@@ -1,6 +1,5 @@
-// import * as Commerce from "@chec/commerce.js";
 const Commerce = require("@chec/commerce.js");
 export const commerce = new Commerce(
-  "pk_232868260a782e22a367e3d1e12bf4eab63797afeaa9b",
+  "pk_23557ffe76e286956121ffc99df3a7410d3224d2530dd",
   true
 );
